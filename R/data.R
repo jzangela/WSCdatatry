4cfda88ee851d481e4921fe5ec6060854e0b2bf9
jzangela <jz3101@tc.columbia.edu>
2024-05-30 09:15:20 +0800

    data description

--- R/data.R
+++ R/data.R
@@ -6,9 +6,6 @@
 #' @format ## `my_pkg_data`
 #' A data frame with 7,240 rows and 60 columns:
 #' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
 #'   ...
 #' }
 #' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
